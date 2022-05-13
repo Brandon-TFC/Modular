20e3676a2ca92d01946910be1998367a3745459c
Brandon <tfcbrandong@gmail.com>
2022-05-12 19:01:39 -0500

    camibios static

--- WebDjango/settings.py
+++ WebDjango/settings.py
@@ -138,18 +138,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
-## DJango buscara en esta ruta, los archivos estaticos que necesite
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+# DJango buscara en esta ruta, los archivos estaticos que necesite
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
